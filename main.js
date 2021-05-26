851c6a9bc4dfb649e86178276d921fc5bfd5be8a
illahopp <84839148+illahopp@users.noreply.github.com>
2021-05-26 13:15:30 +0200

    ADDED

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function() {
+	let newVideo = document.querySelector("#vid")
+	newVideo.addEventListener("ended", function() {
+		this.currentTime = 0
+		this.play()
+	}, false)
+
+	newVideo.play()
+})
